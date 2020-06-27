f29901e093197f18174fe87d6176fdb08c3c24bd
ribeiro-hugo <66832976+ribeiro-hugo@users.noreply.github.com>
2020-06-27 13:48:27 +0100

    Update solution1_sklearn_log_regression.py

--- solution1_sklearn_log_regression.py
+++ solution1_sklearn_log_regression.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from sklearn import model_selection
+from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LogisticRegression
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -11,18 +11,19 @@ x_data = df_train["text"].values
 Y = df_train["target"].values
 
 #Split into training and validation
-x_train, x_validation, y_train, y_validation = model_selection.train_test_split(x_data, Y, test_size=0.2, random_state=7)
+x_train, x_validation, y_train, y_validation = train_test_split(x_data, Y, test_size=0.2, random_state=7)
 
 #Create, train and validate model
 model = Pipeline([('tfidf', TfidfVectorizer()), ('lr', LogisticRegression())])
 model.fit(x_train, y_train)
-pred_train = model.predict(x_train)
-print('training: %.2f' % np.mean(pred_train == y_train))
-pred_val = model.predict(x_validation)
-print('validation: %.2f' % np.mean(pred_val == y_validation))
+trainScore = model.score(x_train,y_train)
+validationScore = model.score(x_validation,y_validation)
+print('training: %.2f' % trainScore)
+print('validation: %.2f' % validationScore)
 
 #Apply model to test dataset
 df_test = pd.read_csv('test.csv', delimiter = ',')
 pred_test = model.predict(df_test['text'].values)
-print('test: %.2f' % np.mean(pred_test == df_test['target'].values))
+testScore = np.mean(pred_test == df_test['target'].values)
+print('test: %.2f' % testScore)
 
